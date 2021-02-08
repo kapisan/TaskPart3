75b5ab845a3bacb210c02da69c549a6c6e423862
kapisan <60970645 + kapisan @ users.noreply.github.com>
2021-02-08 09:54:51 +0900

    story board上の部品とコードの紐付け完了

--- TaskPart3/ViewController.swift
+++ TaskPart3/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var mySwitch1: UISwitch!
+    @IBOutlet private weak var mySwitch2: UISwitch!
+    @IBOutlet private weak var label1: UILabel!
+    @IBOutlet private weak var label2: UILabel!
+    @IBOutlet private weak var totalLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
